5785ef3c1e7c3690075c4fbff8104a9fed08e778
vente16 <jw2@misena.edu.co>
2017-08-28 22:40:55 -0500

    Maquetación del módulo torenos

--- js/rutas.js
+++ js/rutas.js
@@ -17,9 +17,25 @@ app.config(['$locationProvider','$routeProvider', function ($locationProvider, $
     	controller: 'nuevoTorneoCtr', 
     	templateUrl: 'componentes/torneos/nuevoTorneo.html'
     })
+    .when("/solicitudesTorneos", {
+    	templateUrl: "componentes/torneos/solicitudes.html", 
+       //	controller: "controladorPrincipal"
+    })
+    .when("/infoTorneo", {
+        templateUrl: "componentes/torneos/infotorneo.html", 
+       //   controller: "controladorPrincipal"
+    })
+    .when("/cambiarlogo", {
+        templateUrl: "componentes/torneos/cambiarlogo.html", 
+       //   controller: "controladorPrincipal"
+    })
+    .when("/buscarTorneo", {
+        templateUrl: "componentes/torneos/buscarTorneo.html", 
+       //   controller: "controladorPrincipal"
+    })
     .when("/404", {
-    	templateUrl: "componentes/comunes/404.tmpl.html", 
-    	controller: "controladorPrincipal"
+        templateUrl: "componentes/comunes/404.tmpl.html", 
+        controller: "controladorPrincipal"
     })
     .otherwise({redirectTo: '/404'});
    /* login*/
